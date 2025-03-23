00c994302d6f9f366b9a2a1730c7d798b06c147e
Prashanthg01 <prashanthprashanth8585@gmail.com>
2025-03-23 22:34:25 +0530

    updated STATICFILES_DIRS

--- send_sms/settings.py
+++ send_sms/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'sms_app/static')]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
